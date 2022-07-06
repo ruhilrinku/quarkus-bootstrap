e1323f81d2769d77ddcf22974d5a4327fb542149
Nishant Kumar <nishant.kumar@brightlysoftware.com>
2022-07-06 18:38:28 +0530

    Update Liquibase Scripts

--- src/main/resources/db/changelog/DML/01_insert_data_company.sql
+++ src/main/resources/db/changelog/DML/01_insert_data_company.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 --changeset nishant_kumar:insert-data-company-table-3
 
-INSERT INTO company VALUES(nextval('hibernate_sequence'), 'Brightly', 'Brightly', 'Brightly Software India Pvt. Ltd.');
-INSERT INTO company VALUES(nextval('hibernate_sequence'), 'SIEMANS', 'SIEMANS', 'SIEMANS Software India Pvt. Ltd.');
+INSERT INTO company VALUES(nextval('hibernate_sequence'), 'Brightly', 'Brightly', 'Brightly Software Pvt. Ltd.');
+
+--rollback delete from company where id < nextval('hibernate_sequence');
